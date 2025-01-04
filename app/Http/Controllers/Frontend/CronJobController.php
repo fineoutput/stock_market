3fd7e24a7e58e381bed8586f24d34770b40e8cb5
anay <anaypareek@rocketmail.com>
2025-01-04 19:56:45 +0530

    cron job auto add

--- app/Http/Controllers/Frontend/CronJobController.php
+++ app/Http/Controllers/Frontend/CronJobController.php
@@ -67,12 +67,26 @@ class CronJobController extends Controller
         $job1= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_CE_5min';
         $job2= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_PE_5min';
 
-        
-        $job3= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/historical-data-5min';
+        //nifty historic data cron job remove
+        $job3= '0 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/historical-data';
+        $job4= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/historical-data-5min';
+
+         //bank nifty historic data cron job remove
+         $job5= '0 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/bank-historical-data';
+         $job6= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/bank-historical-data-5min';
+
+          //Stock option historic data cron job remove
+        $job7= '0 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/stock-historical-data';
+        $job8= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/stock-historical-data-5min';
 
         $this->deleteCronJob($job1);    
         $this->deleteCronJob($job2);    
         $this->deleteCronJob($job3);    
+        $this->deleteCronJob($job4);    
+        $this->deleteCronJob($job5);    
+        $this->deleteCronJob($job6);    
+        $this->deleteCronJob($job7);    
+        $this->deleteCronJob($job8);    
 
       
 
