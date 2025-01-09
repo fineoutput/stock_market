bd770738460b8369237f9d41a2a0edd91a9f99f2
anay <anaypareek@rocketmail.com>
2025-01-09 19:49:30 +0530

    cron job change

--- app/Http/Controllers/Frontend/CronJobController.php
+++ app/Http/Controllers/Frontend/CronJobController.php
@@ -79,7 +79,7 @@ class CronJobController extends Controller
 
          //bank nifty historic data cron job remove
         //  $job5= '0 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/bank-historical-data';
-         $job6= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/bank-historical-data-5min';
+        //  $job6= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/bank-historical-data-5min';
 
 
          
@@ -87,19 +87,19 @@ class CronJobController extends Controller
         // $job7= '0 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/stock-historical-data';
         // $job8= '*/5 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/stock-historical-data-5min';
 
-        $job9= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_bank_CE_5min';
-        $job10= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_bank_PE_5min';
+        // $job9= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_bank_CE_5min';
+        // $job10= '*/1 * * * 1-5 /usr/bin/curl --silent --compressed https://fineoutput.co.in/stock_market/public/Order/createOrder_bank_PE_5min';
 
         $this->deleteCronJob($job1);    
         $this->deleteCronJob($job2);    
         // $this->deleteCronJob($job3);    
         $this->deleteCronJob($job4);    
         // $this->deleteCronJob($job5);    
-        $this->deleteCronJob($job6);    
+        // $this->deleteCronJob($job6);    
         // $this->deleteCronJob($job7);    
         // $this->deleteCronJob($job8);    
-           $this->deleteCronJob($job9);    
-        $this->deleteCronJob($job10); 
+        //    $this->deleteCronJob($job9);    
+        // $this->deleteCronJob($job10); 
 
       
 
